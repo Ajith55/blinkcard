0c8e95a2bf3b641f443a4ae431fb0f2b44341317
ajith <ajithchowdary96@gmail.com>
2020-11-28 18:16:08 -0500

    added routing for main components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AccountComponent } from './account/account.component';
+import { GetStartedComponent } from './get-started/get-started.component';
+import { HomeComponent } from './home/home.component';
+import { SupportComponent } from './support/support.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', redirectTo: '/home', pathMatch: 'full' },
+{ path: 'home', component: HomeComponent },
+{ path: 'about', component: AboutComponent },
+{ path: 'get-started', component: GetStartedComponent },
+{ path: 'account', component: AccountComponent },
+{ path: 'support', component: SupportComponent },
+
+{ path: '**', redirectTo: '/home' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { GetStartedComponent } from './get-started/get-started.component';
+import { AccountComponent } from './account/account.component';
+import { SupportComponent } from './support/support.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    GetStartedComponent,
+    AccountComponent,
+    SupportComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
